bdb06163f858ad6800427cdf778a48d64369f54b
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2024-08-21 19:29:22 +0600

    Area calculation.

---
+++ areas.js
@@ -0,0 +1,15 @@
+const PI = 3.1416;
+let r = 5;
+let areaOfCircle = PI * r * r;
+console.log(areaOfCircle);
+
+/**
+ * Another Way of Doing This
+ */
+let anotherWayOfArea = Math.PI * Math.pow(r, 2);
+console.log(anotherWayOfArea);
+
+/**
+ * Homework:
+ * Calculate the area of Rectangle, Square, and Triangle.
+ */
