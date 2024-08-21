c346a834714a9b073e7e9720ee01bf92817ab80a
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2024-08-21 19:21:56 +0600

    String Concat.

---
+++ string-concat.js
@@ -0,0 +1,10 @@
+let firstName = 'Shekh Rasel';
+let lastName = 'Masrur Ahmmad';
+
+// Full Name = Shekh Rasel Masrur Ahmmad
+let fullName = firstName + '' + lastName;
+console.log(fullName);
+
+// Full Name Back-Tick
+let fullNameBackTick = `${firstName} ${lastName} is a teacher.`;
+console.log(fullNameBackTick);
