fd7c2720f5526854594a6f708c5cf49b0b500353
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2024-08-21 19:26:39 +0600

    Conditions.

---
+++ conditions.js
@@ -0,0 +1,17 @@
+let money = 20;
+let friendMoney = 5000;
+let biriyani = 550;
+let dalVat = 100;
+let kacchiVaiIsOpen = false;
+if ((money >= biriyani || friendMoney >= biriyani) && kacchiVaiIsOpen == true) {
+	console.log(`We will eat Biriyani.`);
+} else if (money >= dalVat) {
+	console.log(`We will eat Dal-Vat`);
+} else {
+	console.log(`We will drink tea and go home.`);
+}
+
+/**
+ * Home Work:
+ * Make a Grading System
+ */
